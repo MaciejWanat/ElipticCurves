485e05bc76fdd09e6c72d0929899d2678e61a757
Maciej Wanat <maciejwanat@10g.pl>
2019-04-06 17:22:00 +0200

    Calculate delta

---
+++ CW1/krzywaPunkt.py
@@ -0,0 +1,37 @@
+# wylosowanie krzywej + pkt
+ #!/usr/bin/python3
+
+from Crypto.Util import number
+INIT_PRIME_SIZE = 256
+
+def randomP():
+    p = number.getPrime(INIT_PRIME_SIZE)
+
+    while(p % 4 != 3):
+        p = number.getPrime(INIT_PRIME_SIZE)
+    return p
+
+def createElipticCurve(p):
+    getRandom = lambda p: number.getRandomRange(0, p - 1)
+
+    calcDelta = lambda a, b: (4 * pow(a, 3)) + (27 * pow(b, 2))
+    elipticCurve = lambda a, b, x: (pow(x, 3, p) + (a * x) + b) % p
+
+    delta = p
+    while (delta % p == 0):
+        a = getRandom(p)
+        b = getRandom(p)
+        x = getRandom(p)    
+        delta = calcDelta(a, b)  
+    else:
+        return elipticCurve(a, b, x)
+
+def main():
+    p = randomP()
+    print('p: ', p)
+
+    elipticCurve = createElipticCurve(p)
+    print(elipticCurve)
+
+if __name__ == '__main__':
+   main()
